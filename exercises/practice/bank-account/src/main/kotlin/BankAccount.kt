87fb8b85fe3c5a73766db3527207aa79bbff0fd0
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2023-03-08 22:36:10 +0100

    solved bank-account

--- exercises/practice/bank-account/src/main/kotlin/BankAccount.kt
+++ exercises/practice/bank-account/src/main/kotlin/BankAccount.kt
@@ -1,11 +1,17 @@
 class BankAccount {
     // TODO: implement read access to 'balance'
+    private var open = true
+    var balance: Long = 0
+        get() { check(open); return field }
+        private set
 
     fun adjustBalance(amount: Long){
-        TODO("Implement the function to complete the task")
+        // TODO("Implement the function to complete the task")
+        synchronized(this){ balance += amount }
     }
 
     fun close() {
-        TODO("Implement the function to complete the task")
+        // TODO("Implement the function to complete the task")
+        open = false
     }
 }
