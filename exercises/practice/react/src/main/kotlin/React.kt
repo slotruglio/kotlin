f61cda9def5eb2f4749109b6baf1d16c72c304fa
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2023-03-12 11:16:07 +0100

    add solution for react

--- exercises/practice/react/src/main/kotlin/React.kt
+++ exercises/practice/react/src/main/kotlin/React.kt
@@ -1,7 +1,74 @@
+import kotlin.properties.Delegates
+
 class Reactor<T>() {
     // Your compute cell's addCallback method must return an object
     // that implements the Subscription interface.
     interface Subscription {
         fun cancel()
     }
+
+    open inner class InputCell(init: T) {
+        val subscribers = mutableListOf<ComputeCell>()
+        var value = init
+            set(new) {
+                field = new
+                for (sub in subscribers) {sub.update()}
+                if (this !is ComputeCell)
+                    for (sub in subscribers) {sub.callback()}
+            }
+
+        fun subscribe(sub: ComputeCell) = subscribers.add(sub)
+    }
+
+    inner class ComputeCell(private vararg var cells: InputCell, var compute: ((List<T>) -> T)) : InputCell(compute(cells.map { it.value })) {
+        init {
+            cells.map { it.subscribe(this) }
+        }
+        inner class Callback(private var callback: ((T) -> Unit)) : Subscription {
+
+            private var isActive = true
+
+            override fun cancel() {
+                isActive = false
+            }
+
+            operator fun invoke(value: T) {
+                if (isActive)
+                    callback(value)
+            }
+        }
+
+        private val callbacks = mutableListOf<Callback>()
+
+        fun addCallback(callback: ((T) -> Unit)): Callback {
+            val callbackObject = Callback(callback)
+            callbacks.add(callbackObject)
+            return callbackObject
+        }
+
+        private var updateInProgress = false
+
+        private var oldValue: T? = null
+
+        fun update() {
+            // Store the value before the update
+            if (!updateInProgress) {
+                oldValue = value
+                updateInProgress = true
+            }
+            value = compute(cells.map { it.value })
+        }
+
+        fun callback() {
+            if (updateInProgress) {
+                updateInProgress = false
+                if (oldValue != value) {
+                    callbacks.forEach { it(value) }
+                }
+                // The callback of the subscribers should be called too
+                subscribers.forEach { it.callback() }
+            }
+        }
+    }
 }
+
